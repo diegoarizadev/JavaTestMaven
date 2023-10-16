051bf7038ebb1963c4f0b9eff2bf646ae800bd31
n0rf3n <diego.ariza2@gmail.com>
2023-10-16 11:58:29 -0500

    Pruebas para una funcion con varios casos con Junit

--- JavaTest/src/main/java/Utils/StringUtil.java
+++ JavaTest/src/main/java/Utils/StringUtil.java
@@ -4,6 +4,11 @@ public class StringUtil {
 
     public static String repeat(String str, int times) {
 
+
+        if(times < 0){
+            throw new IllegalArgumentException("Negative times not allowed");
+        }
+
         StringBuilder result = new StringBuilder();
 
         for (int i = 0; i < times; i++) {
--- JavaTest/src/test/java/Utils/StringUtilTest.java
+++ JavaTest/src/test/java/Utils/StringUtilTest.java
@@ -6,10 +6,30 @@ import org.junit.Test;
 public class StringUtilTest {
 
     @Test
-    public void testRepeat(){
+    public void repeat_string_one(){
+
+        Assert.assertEquals("Hola", StringUtil.repeat("Hola", 1));
+
+    }
+
+    @Test
+    public void repeat_string_multiple_times(){
 
         Assert.assertEquals("HolaHolaHola", StringUtil.repeat("Hola", 3));
-        Assert.assertEquals("Hola", StringUtil.repeat("Hola", 1));
+
+    }
+
+    @Test
+    public void repeat_string_zero_times(){
+
+        Assert.assertEquals("", StringUtil.repeat("Hola", 0));
+
+    }
+
+    @Test(expected =  IllegalArgumentException.class)
+    public void repeat_string_negative_times(){
+
+        Assert.assertEquals("", StringUtil.repeat("Hola", -1));
 
     }
 }
